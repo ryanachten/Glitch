97c07ac7f82c94e824e33868f9de0c6f6edc5a8d
RyanAchten <ryanachten@gmail.com>
2020-05-24 11:25:37 +1200

    Add PoC online glitch method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,34 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  image: any;
+  originalImage: any;
 
   uploadImage(event) {
     const files = event.target.files;
     const fileReader: FileReader = new FileReader();
     fileReader.onload = e => {
-      this.image = e.target.result;
+      const encodedUri = e.target.result as string;
+      const dataHeader = "data:image/jpeg;base64,";
+      const decodedUri = atob(encodedUri.replace(dataHeader, ""));
+      const modifiedImage = decodedUri.toString().replace(/bc/g, "cd");
+      const encodedModifiedImage = `${dataHeader}${btoa(modifiedImage)}`;
+      this.originalImage = encodedModifiedImage;
+
+      this.generateCanvas();
     };
     fileReader.readAsDataURL(files[0]);
+  }
+
+  generateCanvas() {
+    const canvas = document.querySelector("canvas");
+    const ctx = canvas.getContext("2d");
+    const image = new Image();
+    image.onload = () => {
+      canvas.height = image.height;
+      canvas.width = image.width;
+      ctx.drawImage(image, 0, 0);
+      // console.log(canvas.toDataURL());
+    };
+    image.src = this.originalImage;
   }
 }
