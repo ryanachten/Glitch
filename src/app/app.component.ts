6cc271a08e8f531f56f5a4bf72a4ef40a0a99c95
RyanAchten <ryanachten@gmail.com>
2020-05-23 15:07:49 +1200

    Display image based on file load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,17 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  // image: any;
+  image: any;
 
   uploadImage(event) {
     const files = event.target.files;
-    console.log("files", files);
+    const fileReader: FileReader = new FileReader();
+    fileReader.onload = e => {
+      this.image = e.target.result;
+    };
+    fileReader.readAsDataURL(files[0]);
   }
 }
