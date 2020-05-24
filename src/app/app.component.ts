9db1e0dba930e128353e3a893dba61f333a56c14
RyanAchten <ryanachten@gmail.com>
2020-05-24 12:40:02 +1200

    Add generateRandomQuery method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,33 @@ import { Component } from "@angular/core";
 export class AppComponent {
   originalImage: any;
 
+  generateRandomQuery(imageBody: string) {
+    const substrLength = Math.floor(Math.random() * 10);
+    const startIndex = Math.floor(
+      Math.random() * (imageBody.length - substrLength)
+    );
+    const queryStr = imageBody.substr(startIndex, substrLength);
+    return new RegExp(queryStr, "g");
+  }
+
   uploadImage(event) {
     const files = event.target.files;
     const fileReader: FileReader = new FileReader();
     fileReader.onload = e => {
-      const encodedUri = e.target.result as string;
+      const encodedUri = e.target.result.toString();
       const dataHeader = "data:image/jpeg;base64,";
       const decodedUri = atob(encodedUri.replace(dataHeader, ""));
-      const modifiedImage = decodedUri.toString().replace(/bc/g, "cd");
-      const encodedModifiedImage = `${dataHeader}${btoa(modifiedImage)}`;
-      this.originalImage = encodedModifiedImage;
+      const replaceRegex = this.generateRandomQuery(decodedUri);
+      console.log("replaceRegex", replaceRegex);
+      const matches = decodedUri.match(replaceRegex);
+      if (matches && matches.length) {
+        console.log("matches", matches.length);
+        const modifiedImage = decodedUri.replace(replaceRegex, "cd");
+        const encodedModifiedImage = `${dataHeader}${btoa(modifiedImage)}`;
+        this.originalImage = encodedModifiedImage;
 
-      this.generateCanvas();
+        this.generateCanvas();
+      }
     };
     fileReader.readAsDataURL(files[0]);
   }
@@ -32,7 +47,6 @@ export class AppComponent {
       canvas.height = image.height;
       canvas.width = image.width;
       ctx.drawImage(image, 0, 0);
-      // console.log(canvas.toDataURL());
     };
     image.src = this.originalImage;
   }
