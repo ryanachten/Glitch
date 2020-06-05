f53a69fe0f6f479934da8d9a4ba86570fa5b57d1
Ryan Achten <ryan.achten@akqa.com>
2020-06-05 16:30:05 +1200

    Add getMutatorById to glitch service

--- src/app/services/glitch.service.ts
+++ src/app/services/glitch.service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from "@angular/core";
 import { SwapImageData } from "../utilities/glitches/SwapImageData";
 import { FindAndReplace } from "../utilities/glitches/FindAndReplace";
+import { MutationId, Mutator } from "../models";
 
 @Injectable({
   providedIn: "root",
 })
 export class GlitchService {
+  mutatorHash = {
+    [MutationId.FindAndReplace]: FindAndReplace,
+    [MutationId.SwapImageData]: SwapImageData,
+  };
+
   constructor() {}
 
-  seedReplaceQuery = FindAndReplace.seed;
-
-  findAndReplace = FindAndReplace.exec;
-
-  seedSwapImageMatrix = SwapImageData.seed;
-
-  swapImageMatrix = SwapImageData.exec;
+  public getMutatorById(id: MutationId): Mutator {
+    const Mutator = this.mutatorHash[id];
+    return new Mutator();
+  }
 }
