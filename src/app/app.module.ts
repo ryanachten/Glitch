669c4db86cc23c670c3f4350c1de8413063ff5a6
Ryan Achten <ryan.achten@akqa.com>
2020-06-06 13:57:07 +1200

    Add init ant design config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,24 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 
+import { NzButtonModule } from "ng-zorro-antd/button";
+
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { CanvasComponent } from './components/canvas/canvas.component';
-import { GalleryComponent } from './components/gallery/gallery.component';
-import { FindAndReplaceComponent } from './components/mutations/find-and-replace/find-and-replace.component';
-import { SwapImageDataComponent } from './components/mutations/swap-image-data/swap-image-data.component';
+import { CanvasComponent } from "./components/canvas/canvas.component";
+import { GalleryComponent } from "./components/gallery/gallery.component";
+import { FindAndReplaceComponent } from "./components/mutations/find-and-replace/find-and-replace.component";
+import { SwapImageDataComponent } from "./components/mutations/swap-image-data/swap-image-data.component";
 
 @NgModule({
-  declarations: [AppComponent, CanvasComponent, GalleryComponent, FindAndReplaceComponent, SwapImageDataComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent,
+    CanvasComponent,
+    GalleryComponent,
+    FindAndReplaceComponent,
+    SwapImageDataComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, NzButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
