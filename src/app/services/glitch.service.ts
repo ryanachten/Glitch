d1a40900f7a324b846611137c017174cea8eae06
Ryan Achten <ryan.achten@akqa.com>
2020-06-13 09:51:55 +1200

    Add getUrlFromMutations method to glitch service

--- src/app/services/glitch.service.ts
+++ src/app/services/glitch.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from "@angular/core";
-import { MutationId as Id, Mutator } from "../models";
+import { MutationId as Id, Mutator, Mutation } from "../models";
 import { FindAndReplaceComponent } from "../components/mutations/find-and-replace/find-and-replace.component";
 import { SwapImageDataComponent } from "../components/mutations/swap-image-data/swap-image-data.component";
 import { SettingsService } from "./settings.service";
+import { EncodingService } from "./encoding.service";
 
 @Injectable({
   providedIn: "root",
@@ -13,10 +14,25 @@ export class GlitchService {
     [Id.SwapImageData]: SwapImageDataComponent,
   };
 
-  constructor(private settings: SettingsService) {}
+  constructor(
+    private settings: SettingsService,
+    private encoding: EncodingService
+  ) {}
 
   public getMutatorById(id: Id): Mutator {
     const Mutator = this.mutatorHash[id];
     return new Mutator(this.settings);
+  }
+
+  public async getUrlFromMutations(mutations: Mutation[]): Promise<string> {
+    let imageData = this.encoding.decodeData(this.settings.originalImage);
+
+    mutations.forEach((mutation: Mutation) => {
+      const Mutator: Mutator = this.getMutatorById(mutation.id);
+      const { updatedImage } = Mutator.exec(imageData, mutation);
+      imageData = updatedImage;
+    });
+    const encodedUri = this.encoding.encodeData(imageData);
+    return encodedUri;
   }
 }
