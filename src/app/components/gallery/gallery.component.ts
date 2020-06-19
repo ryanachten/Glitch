422d3a0be879464b9db91902c8c110113b1378ef
Ryan Achten <ryanachten@gmail.com>
2020-06-20 10:22:35 +1200

    Reset gallery between visits to avoid incorrect original mapping

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -29,6 +29,7 @@ export class GalleryComponent implements OnInit {
   ngOnInit() {
     const image = this.route.data["value"]["image"];
     this.originalImage = image;
+    this.resetEvolution();
   }
 
   resetEvolution() {
