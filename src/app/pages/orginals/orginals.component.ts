4d781d19a325036ca32d0666c071c42f632987a7
Ryan Achten <ryanachten@gmail.com>
2020-06-21 10:37:26 +1200

    Store size as bytes to format dynamically

--- src/app/pages/orginals/orginals.component.ts
+++ src/app/pages/orginals/orginals.component.ts
@@ -21,6 +21,19 @@ export class OrginalsComponent implements OnInit, PageTemplate {
 
   ngOnInit() {}
 
+  // Via https://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
+  formatBytes(bytes, decimals = 2): string {
+    if (bytes === 0) return "0 Bytes";
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ["Bytes", "KB", "MB"];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+  }
+
   async getImageDimensions(
     encodedUri: string
   ): Promise<{ height: number; width: number }> {
